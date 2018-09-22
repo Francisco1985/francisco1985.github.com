27546aa8a9c090ba9fa90395209e31d94d5e5395
Francisco1985 <42693901+Francisco1985@users.noreply.github.com>
2018-09-22 12:12:46 -0300

    iniciar

---
+++ Proyecto2/assets/scripts/main.js
@@ -0,0 +1,37 @@
+// alert("Hola Mundo JS");
+// console.log("Hola Mundo JS en consola");     
+// let element= document.querySelector(".fa-star");
+
+let elementlist= document.querySelectorAll(".fa-star");
+
+
+     elementlist.forEach(function(element){
+     element.addEventListener ("click",function() {
+      alert ("Pelicula buena");
+     element.classList.remove("far");
+      element.classList. add ("fas");
+     
+     })
+      
+ });
+
+let closes = document.querySelectorAll(".close");
+let content = document.querySelector(".content");
+
+closes.forEach(function(close){
+    close.addEventListener("click",function(ev) {
+        ev.preventDefault();
+        content.classList.remove("animated");
+        content.classList.remove("fadeInRight");
+        content.classList.add("animated");
+        content.classList.add("fadeOutRight");
+        setTimeout(() => {
+            
+         location.href="file:///C:/Users/Mar%C3%ACacarolina/Documents/aiep/Proyecto2/index.html";  
+        }, 1000);
+
+        
+
+    });
+})
+ 
